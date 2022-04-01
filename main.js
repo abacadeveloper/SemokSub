c7da3a92af93ebc0ef8677afbeb5a5d314445f86
Alberto Baca <60315845+abacadeveloper@users.noreply.github.com>
2022-04-01 11:46:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -74,4 +74,24 @@ sr.reveal(`.footer-container-inner > div, .footer-separator, copyright`, {
 
 //finish of function above
 
+let mouseCursor = document.querySelector('.cursor');
+let naviLinks = document.querySelectorAll('.navi-links li');
 
+window.addEventListener('mousemove' ,cursor);
+
+function cursor(e){
+  mouseCursor.style.top = e.pageY + 'px';
+  mouseCursor.style.left = e.pageX + 'px';
+}
+
+navLinks.forEach(link => {
+  link.addEventListener('mouseleave', () => {
+    mouseCursor.classList.remove('link-grow');
+    link.classList.remove('hovered-link');
+  });
+  link.addEventListener('mouseover', () => {
+    mouseCursor.classList.add('link-grow');
+    link.classList.add('hovered-link');
+  });
+  
+});
